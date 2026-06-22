83b3856496693927ff28325f66626d002cd1a333
AllF1RE <111210671+AllFiRE0@users.noreply.github.com>
2026-06-22 10:38:53 +0300

    Create QQCapture.java

---
+++ src/main/java/com/qqcapture/QQCapture.java
@@ -0,0 +1,121 @@
+package com.qqcapture;
+
+import com.qqcapture.commands.QQCaptureCommand;
+import com.qqcapture.config.ConfigManager;
+import com.qqcapture.config.LanguageManager;
+import com.qqcapture.managers.*;
+import com.qqcapture.integration.PlaceholderAPIHook;
+import com.qqcapture.integration.VaultIntegration;
+import com.qqcapture.integration.WorldGuardIntegration;
+import com.qqcapture.listeners.PlayerListener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class QQCapture extends JavaPlugin {
+    private static QQCapture instance;
+    
+    private ConfigManager configManager;
+    private LanguageManager languageManager;
+    private SessionManager sessionManager;
+    private BossBarManager bossBarManager;
+    private CommandManager commandManager;
+    private PlaceholderManager placeholderManager;
+    private ConditionManager conditionManager;
+    private RegionManager regionManager;
+    
+    // Integrations
+    private VaultIntegration vaultIntegration;
+    private WorldGuardIntegration worldGuardIntegration;
+    private PlaceholderAPIHook placeholderAPIHook;
+    
+    @Override
+    public void onEnable() {
+        instance = this;
+        
+        // Save default config
+        saveDefaultConfig();
+        
+        // Initialize managers
+        initializeManagers();
+        
+        // Register commands
+        getCommand("qqcapture").setExecutor(new QQCaptureCommand());
+        
+        // Register listeners
+        getServer().getPluginManager().registerEvents(new PlayerListener(), this);
+        
+        // Check dependencies
+        checkDependencies();
+        
+        getLogger().info("QQCapture v" + getDescription().getVersion() + " enabled!");
+        getLogger().info("Debug mode: " + configManager.isDebug());
+    }
+    
+    private void initializeManagers() {
+        this.configManager = new ConfigManager(this);
+        this.languageManager = new LanguageManager(this);
+        this.sessionManager = new SessionManager(this);
+        this.bossBarManager = new BossBarManager(this);
+        this.commandManager = new CommandManager(this);
+        this.placeholderManager = new PlaceholderManager(this);
+        this.conditionManager = new ConditionManager(this);
+        this.regionManager = new RegionManager(this);
+    }
+    
+    private void checkDependencies() {
+        // Check Vault
+        if (getServer().getPluginManager().getPlugin("Vault") != null) {
+            this.vaultIntegration = new VaultIntegration(this);
+            getLogger().info("Vault integration enabled!");
+        } else {
+            getLogger().warning("Vault not found! Economy features will be disabled.");
+        }
+        
+        // Check WorldGuard
+        if (getServer().getPluginManager().getPlugin("WorldGuard") != null) {
+            this.worldGuardIntegration = new WorldGuardIntegration(this);
+            getLogger().info("WorldGuard integration enabled!");
+        } else {
+            getLogger().warning("WorldGuard not found! Region protection features will be disabled.");
+        }
+        
+        // Check PlaceholderAPI
+        if (getServer().getPluginManager().getPlugin("PlaceholderAPI") != null) {
+            this.placeholderAPIHook = new PlaceholderAPIHook(this);
+            getLogger().info("PlaceholderAPI integration enabled!");
+        } else {
+            getLogger().warning("PlaceholderAPI not found! Custom placeholders will be limited.");
+        }
+    }
+    
+    @Override
+    public void onDisable() {
+        // Stop all active sessions
+        if (sessionManager != null) {
+            sessionManager.stopAllSessions();
+        }
+        
+        // Clear bossbars
+        if (bossBarManager != null) {
+            bossBarManager.clearAllBossBars();
+        }
+        
+        getLogger().info("QQCapture disabled!");
+    }
+    
+    public static QQCapture getInstance() {
+        return instance;
+    }
+    
+    // Getters
+    public ConfigManager getConfigManager() { return configManager; }
+    public LanguageManager getLanguageManager() { return languageManager; }
+    public SessionManager getSessionManager() { return sessionManager; }
+    public BossBarManager getBossBarManager() { return bossBarManager; }
+    public CommandManager getCommandManager() { return commandManager; }
+    public PlaceholderManager getPlaceholderManager() { return placeholderManager; }
+    public ConditionManager getConditionManager() { return conditionManager; }
+    public RegionManager getRegionManager() { return regionManager; }
+    public VaultIntegration getVaultIntegration() { return vaultIntegration; }
+    public WorldGuardIntegration getWorldGuardIntegration() { return worldGuardIntegration; }
+    public PlaceholderAPIHook getPlaceholderAPIHook() { return placeholderAPIHook; }
+}
